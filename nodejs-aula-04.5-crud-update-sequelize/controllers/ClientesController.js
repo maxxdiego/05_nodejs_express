a659fba613c75f06f45c60bbb9a9ff1337ae3228
maxx.diego <77diego.max@gmail.com>
2024-01-19 14:15:56 -0300

    @maxxdiego

---
+++ nodejs-aula-04.5-crud-update-sequelize/controllers/ClientesController.js
@@ -0,0 +1,68 @@
+import express from "express"
+import Cliente from "../models/Cliente.js"
+const router = express.Router()
+
+// ROTA CLIENTES
+router.get("/clientes", function(req, res){
+    Cliente.findAll().then(clientes => {
+        res.render("clientes", {
+            clientes: clientes
+        })
+    })
+})
+
+// ROTA DE CADASTRO DE CLIENTES
+router.post("/clientes/new", (req, res) => {
+    const nome = req.body.nome
+    const cpf = req.body.cpf
+    const endereco = req.body.endereco
+    Cliente.create({
+        nome : nome,
+        cpf : cpf,
+        endereco : endereco
+    }).then(() => {
+        res.redirect("/clientes")
+    })
+})
+
+// ROTA DE EXCLUSÃO DE CLIENTES
+router.get("/clientes/delete/:id", (req, res) => {
+    const id = req.params.id
+    Cliente.destroy({
+        where: {
+            id : id
+        }
+    }).then(() => {
+        res.redirect("/clientes")
+    })
+})
+
+// ROTA DE EDIÇÃO DE CLIENTES
+router.get("/clientes/edit/:id", (req, res) => {
+    const id = req.params.id
+    Cliente.findByPk(id).then(function(cliente) {
+        res.render("clienteEdit", {
+            cliente : cliente
+        })
+    })
+})
+
+// ROTA DE ALTERAÇÃO DE CLIENTES
+router.post("/clientes/update/:id", (req, res) => {
+    const id = req.body.id
+    const nome = req.body.nome
+    const cpf = req.body.cpf
+    const endereco = req.body.endereco
+    Cliente.update(
+        {
+            nome : nome,
+            cpf : cpf,
+            endereco : endereco
+        },
+        {where: {id : id}}
+    ).then(() => {
+        res.redirect("/clientes")
+    })
+})
+
+export default router //exportando o módulo "router"
