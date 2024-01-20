ae7937d79e0018a97e741f8c5621f2abbdc34fed
maxx.diego <77diego.max@gmail.com>
2024-01-20 11:06:14 -0300

    @maxxdiego

---
+++ nodejs-aula-04B.5-crud-update-mongoose/index.js
@@ -0,0 +1,41 @@
+// Importando o Express
+import express from "express" 
+// Iniciando o Express
+const app = express()
+// Importando a ODM Mongoose
+import mongoose from "mongoose" 
+// Importando os Controllers (onde estão as rotas)
+import ClientesController from "./controllers/ClientesController.js"
+import PedidosController from "./controllers/PedidosController.js"
+import ProdutosController from "./controllers/ProdutosController.js"
+
+// Permite receber dados vindo de formulários
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Iniciando conexão com o banco de dados do MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/loja")
+
+// Define o EJS como Renderizador de páginas
+app.set('view engine', 'ejs')
+// Define o uso da pasta "public" para uso de arquivos estáticos
+app.use(express.static('public'))
+// Definindo o uso das rotas dos Controllers
+app.use("/", ClientesController)
+app.use("/", PedidosController)
+app.use("/", ProdutosController)
+
+// ROTA PRINCIPAL
+app.get("/",function(req,res){
+    res.render("index")
+})
+
+// INICIA O SERVIDOR NA PORTA 8080
+app.listen(8080,function(erro){
+    if(erro) {
+        console.log("Ocorreu um erro!")
+
+    }else{
+        console.log("Servidor iniciado com sucesso!")
+    }
+})
