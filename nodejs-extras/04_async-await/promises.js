30239d151b7657787eed2e1b5b707290f95945d1
Diego Max <126022710+maxxdiego@users.noreply.github.com>
2024-11-24 14:36:31 -0300

    Update promises.js

--- nodejs-extras/04_async-await/promises.js
+++ nodejs-extras/04_async-await/promises.js
@@ -9,7 +9,7 @@ function pegarId(){
 function buscarEmailNoBanco(id){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve("victorlima@guia.com.br")
+            resolve("nome@email.com.br")
         },2000);
     })
 }
@@ -19,7 +19,7 @@ function enviarEmail(corpo, para){
         setTimeout(() => {  
             var deuErro = false;
             if(!deuErro){
-                resolve({time: 6, to: "victor@udemy.com"}) // Promessa OK!
+                resolve({time: 6, to: "diego@email.com"}) // Promessa OK!
             }else{
                 reject("Fila cheia") // Foi mal, eu falhei :(
             }
